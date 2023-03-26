1cf3219e4aa122461b515f355a2e3420b6e37722
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2023-03-26 10:51:50 +0500

    Add files via upload

---
+++ schemas/index.js
@@ -0,0 +1,8 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import RanaRabees from './userdata'
+
+
+export const schemaTypes = [post, author, category, blockContent, RanaRabees]
---
+++ schemas/userdata.js
@@ -0,0 +1,26 @@
+// export default {
+//     username: 'RanaRabees',
+//     email: 'rabeeshussainranaverse@gmail.com',
+//     password: 'rabeeeshussainranaVerse',
+//     fields: [
+//         {
+//             username: 'username',
+//             email: 'email',
+//             password: 'password',
+//         }
+//     ]
+// }
+
+// schemas/userdata.js
+export default {
+  name: 'RanaRabees',
+  type: 'document',
+  title: 'RanaRabees',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    }
+  ]
+}
